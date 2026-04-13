444bade88d108ac8fbbc5059b303ba00d52aa9ee
NAZ ÇETİN <nazcetin@192.168.1.101>
2026-04-13 23:12:16 +0300

    app ve homepage temizlendi

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,5 @@
 import HomePage from "./pages/HomePage";
 
 export default function App() {
-  return (
-    <div>
-      <div style={{ background: "red", color: "#fff", padding: 10 }}>
-        APP OK
-      </div>
-
-      <HomePage />
-    </div>
-  );
+  return <HomePage />;
 }
