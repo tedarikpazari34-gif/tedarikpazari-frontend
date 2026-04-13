c9c901927394a10ee6b4c6707bce0712c2565c93
NAZ ÇETİN <nazcetin@192.168.1.101>
2026-04-13 22:46:42 +0300

    test

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,7 @@
 import { Helmet } from "react-helmet-async";
 import { Link } from "react-router-dom";
 import PopularSectors from "../components/PopularSectors";
-
+// <PopularSectors />
 export default function HomePage() {
   return (
     <>
