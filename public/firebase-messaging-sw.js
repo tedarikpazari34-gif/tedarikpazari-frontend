709ad38b1196ed53a0b4d5b2ba9ba39c64cd211d
NAZ ÇETİN <nazcetin@192.168.1.104>
2026-07-22 00:56:09 +0300

    Match Firebase service worker version

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts("https://www.gstatic.com/firebasejs/12.1.0/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/12.1.0/firebase-messaging-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/12.16.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/12.16.0/firebase-messaging-compat.js");
 
 firebase.initializeApp({
   apiKey: "AIzaSyDmkt_dIxdpujJbsA-Z4nYp2ZNsW2MUpT8",
