cfaad35b8d95002cee5e096d8dc6b176faa19180
NAZ ÇETİN <nazcetin@192.168.1.102>
2026-08-10 15:03:46 +0300

    Add Android app with Capacitor

---
+++ android/app/src/main/java/com/tedarikpazari34/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.tedarikpazari34.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.tedarikpazari34.app',
+  appName: 'Tedarik Pazarı',
+  webDir: 'dist'
+};
+
+export default config;
